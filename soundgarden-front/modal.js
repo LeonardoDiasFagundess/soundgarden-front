9e30bc6b68ac659588dcf35b5635a65276ffce45
LeonardoDiasFagundess <37003510+LeonardoDiasFagundess@users.noreply.github.com>
2022-04-02 09:29:59 -0300

    Add files via upload

---
+++ soundgarden-front/modal.js
@@ -0,0 +1,14 @@
+const modalContainer = document.querySelector("#modal-container")
+const links = document.querySelectorAll(".btn.btn-primary") 
+
+for(link of links) {
+    link.addEventListener("click", () => {
+        modalContainer.classList.add("show")
+    })
+}
+
+const btnFechar = document.querySelector('.btn.btn-danger')
+
+btnFechar.addEventListener('click',() => {
+    modalContainer.classList.remove('show')
+})
